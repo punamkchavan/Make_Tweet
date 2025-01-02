745a4ab480af9f6a7b1e05231de0fdb438e4a81f
punamkchavan <cpunam1123@gmail.com>
2025-01-02 23:06:25 +0530

    update changes

--- commenthead/tweet/models.py
+++ commenthead/tweet/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Tweet(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     text=models.TextField(max_length=250)
-    photo=models.ImageField(upload_to='photos/',blank=True,null=True)
+    photo=models.ImageField(upload_to='photo/',blank=True,null=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     
