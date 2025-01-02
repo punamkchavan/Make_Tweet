90793f1b68a1f3643edb17e2116915d080f25eae
punamkchavan <cpunam1123@gmail.com>
2025-01-02 23:39:30 +0530

    errors solved

--- commenthead/commenthead/settings.py
+++ commenthead/commenthead/settings.py
@@ -124,12 +124,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/' 
+MEDIA_URL = 'media/' 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/static/'  
+STATIC_URL = 'static/'  
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-LOGIN_URL='/accounts/login'
-LOGIN_REDIRECT_URL='/tweet/'
-LOGOUT_REDIRECT_URL='/tweet/'
+LOGIN_URL='accounts/login'
+LOGIN_REDIRECT_URL='tweet/'
+LOGOUT_REDIRECT_URL='tweet/'
